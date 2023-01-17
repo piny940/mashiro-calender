90ad77280f4359f2bc25744b36e3c3d1a1d84eb4
piny940 <an.s.shoma@gmail.com>
2023-01-17 11:05:54 +0900

    ユーザー名を入力するフォームを作成

--- mashiro-calender.py
+++ mashiro-calender.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
   name = 'Hello world'
   return render_template('index.html', name=name)
+
+@app.route('/calender', methods=['POST'])
+def calender():
+  if request.method == 'POST':
+    print(request.form)
+    print(request.form['username'])
+  
+  return render_template('index.html', name='form successful?')
 
 if __name__ == '__main__':
   app.run(debug=True)
