228aba35ece933b84034f722c18a8c5beb90e621
piny940 <an.s.shoma@gmail.com>
2023-01-17 21:48:22 +0900

    twitter apiを使えるようにする

--- mashiro-calender.py
+++ mashiro-calender.py
@@ -1,20 +1,34 @@
 from flask import Flask, render_template, request
+import tweepy
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 app = Flask(__name__)
 
+consumer_key = os.getenv('API_KEY')
+consumer_secret = os.getenv('API_KEY_SECRET')
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+bearer_token = os.getenv('BEARER_TOKEN')
+
+client = tweepy.Client(bearer_token)
+
 @app.route('/')
 def index():
   name = 'Hello world'
+  response = client.get_user(username='PoporonPoyopoyo')
+  user_id = response.data.id
+  print(user_id)
+  print(response.data.name)
+  response = client.search_recent_tweets(f'#なぎのらいぶ from:{user_id}')
+  tweets = response.data
+  print(response.data)
+    
   return render_template('index.html', name=name)
 
 @app.route('/calender', methods=['POST'])
 def calender():
-  if request.method == 'POST':
-    print(request.form)
-    print(request.form['username'])
   
   return render_template('index.html', name='form successful?')
 
